6d7c1fc4c655c6ce11da6fa69d31855e66f1c9aa
Delfifini <jq.delfina@gmail.com>
2024-07-02 19:09:51 -0300

    corrección condicionales

--- js/app.js
+++ js/app.js
@@ -11,19 +11,26 @@ function totalFlor(persona, gFPersona){
 
 function calcular(){
     let gPPersona = 1.25;
-    let elegir = prompt(`Bienvenido al calculador de cannabis de cocina! \n Si tenés prensado elegí "1", si tenés flores elegí "2":`);
-
+    let elegir = prompt(`Bienvenido al calculador de cannabis para cocinar! \n Primero contanos, vas a cocinar con: \n 1. prensadito \n 2. flores \n`);
     if (elegir === '1'){
-        let persona = parseFloat(prompt(`Uy que lástima, pero, ¿para cuántas personas querés cocinar?`));
+        let persona = parseFloat(prompt(`Se hace lo que se puede, pero, ¿para cuántas personas querés cocinar? \n \n (Valores calculados en base a una persona de aprox 165cm y 70kg.)`));
+        if (isNaN(persona) || persona <= 0){
+            alert(`Alguien ya está medio loco, ingresá un valor válido por favor.`)
+        } else {
         let totalCanna = totalPrens(persona, gPPersona);
-        alert(`Si vas a cocinar para ${persona} personas, necesitás ${totalCanna} gramos de prensado`); 
+        alert(`Si vas a cocinar para ${persona} personas, necesitás ${totalCanna} gramos de prensado.\n \n Te recomendamos descartar el producto una vez hecha la extracción ya que suele contener muchas partes molestas al consumir...\n y que asquito e.e `); 
+        }
     } else if(elegir === '2'){
         let gFPersona = 0.5;
-        let persona = parseFloat(prompt(`Bien ahí! ¿Para cuántas personas querés cocinar?`));
+        let persona = prompt(`Bien ahí! ¿Para cuántas personas querés cocinar? \n \n (Valores calculados en base a una persona de aprox 165cm y 70kg.)`);
+        if (isNaN(persona) || persona <= 0){
+            alert(`Alguien ya está medio loco, ingresá un valor válido por favor.`)
+        } else {
         let totalCanna = totalFlor(persona, gFPersona)
-        alert(`Si vas a cocinar para ${persona} personas necesitás ${totalCanna} gramos de flores`)
+        alert(`Si vas a cocinar para ${persona} personas necesitás ${totalCanna} gramos de flores. \n \n Si son de buen pegue seguramente deberías usar un poquito menos y viceversa. Podés usar manicura fina reemplazando 1g de flores por 1.8g de manicura`)
+        }
     } else {
-        alert(`Alguien ya está re loco, ingresá un valor válido por favor.`)
+        alert(`Alguien ya está medio loco, ingresá un valor válido por favor.`)
     }
 
 }
