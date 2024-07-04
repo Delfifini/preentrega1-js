718813dd9256d8c9ffa9d87473c0b84f9a932dfb
Delfifini <jq.delfina@gmail.com>
2024-07-04 10:18:53 -0300

    cambio alert menos agresivo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-//* Esto es un calculador de cannabis por persona para cocinar
-
 function totalPrens(persona){
     let gPPersona = 1.25;
     return persona * gPPersona;
@@ -12,11 +10,11 @@ function totalFlor(persona){
 
 
 function calcular(){
-    let elegir = prompt(`Bienvenido al calculador de cannabis para cocinar! \n Primero contanos, vas a cocinar con: \n 1. prensadito \n 2. flores \n`);
-    if (elegir === '1'){
+    let elegir = prompt(`Bienvenido al calculador de cannabis para cocinar! \n Primero contanos, vas a cocinar con: \n 1. prensadito \n 2. flores \n (Ingresá el número correspondiente a la opción correcta)`);
+    if (elegir === '1' ){
         let persona = prompt(`Se hace lo que se puede, pero, ¿para cuántas personas querés cocinar? \n \n (Valores calculados en base a una persona de aprox 165cm y 70kg.)`);
         if (isNaN(persona) || persona <= 0){
-            alert(`Alguien ya está medio loco, ingresá un valor válido por favor.`)
+            alert(`Ingresá un valor numérico por favor.`)
         } else {
         let totalCanna = totalPrens(persona);
         alert(`Si vas a cocinar para ${persona} personas, necesitás ${totalCanna} gramos de prensado.\n \n Te recomendamos descartar el producto una vez hecha la extracción ya que suele contener muchas partes molestas al consumir...\n y que asquito e.e `); 
@@ -24,15 +22,14 @@ function calcular(){
     } else if(elegir === '2'){
         let persona = prompt(`Bien ahí! ¿Para cuántas personas querés cocinar? \n \n (Valores calculados en base a una persona de aprox 165cm y 70kg.)`);
         if (isNaN(persona) || persona <= 0){
-            alert(`Alguien ya está medio loco, ingresá un valor válido por favor.`)
+            alert(`Ingresá un valor numérico por favor.`)
         } else {
         let totalCanna = totalFlor(persona)
         alert(`Si vas a cocinar para ${persona} personas necesitás ${totalCanna} gramos de flores. \n \n Si son de buen pegue seguramente deberías usar un poquito menos y viceversa. Podés usar manicura fina reemplazando 1g de flores por 1.8g de manicura`)
         }
     } else if(elegir !== null && elegir !== undefined){
-        alert(`Alguien ya está medio loco, ingresá un valor válido por favor.`)
+        alert(`Ingresá un valor numérico por favor.`)
     }
-
 }
 
 calcular();
